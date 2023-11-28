0d36fabd36f84357977f840f072018e1eea0d53f
root <root@ADNS-Alex-XPS.advanceddns.com>
2023-11-28 08:53:54 -0500

    Program js file

---
+++ app.js
@@ -0,0 +1,52 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const compScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const reult_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            console.log("User Wins!");
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            console.log("User loses!");
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            console.log("tie!");
+            break;
+    }
+}
+
+function main() {
+    rock_div.addEventListener('click', function() {
+        game("r");
+    })
+
+    paper_div.addEventListener('click', function() {
+        game("p");
+    })
+
+    scissors_div.addEventListener('click', function() {
+        game("s");
+    })
+}
+
+main();

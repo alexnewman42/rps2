03585a4ff3bc6b3bef83646bdfb3d08fdef4a09f
root <root@ADNS-Alex-XPS.advanceddns.com>
2023-11-28 22:28:07 -0500

    Bult win function

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
-const userScore_span = document.getElementById("user-score");
-const compScore_span = document.getElementById("comp-score");
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("userscore");
+const compScore_span = document.getElementById("compscore");
 const scoreBoard_div = document.querySelector(".score-board");
-const reult_div = document.querySelector(".result");
+const result_p= document.querySelector(".result" > p);
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -14,23 +14,38 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
+function win(user, computer) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = computerScore;
+    result_p.innerHTML = user + " beats " + computer + ". You win!"
+}
+
+function loose() {
+    computerScore++;
+}
+
+function draw() {
+
+}
+
 function game(userChoice) {
     const computerChoice = getComputerChoice();
     switch (userChoice + computerChoice) {
         case "rs":
         case "pr":
         case "sp":
-            console.log("User Wins!");
+            win(userChoice, computerChoice);
             break;
         case "rp":
         case "ps":
         case "sr":
-            console.log("User loses!");
+            loose();
             break;
         case "rr":
         case "pp":
         case "ss":
-            console.log("tie!");
+            draw();
             break;
     }
 }
